34a534855b47cb416bcbea8b4d87e3752612d880
Felipe <felipe@felipe-vm.(none)>
2016-02-14 04:41:00 -0300

    Agregado condifional para no imprimir producos que no tienen cuantidades configuradas

--- report/reporte.py
+++ report/reporte.py
@@ -6,14 +6,14 @@ class ParticularReport(models.AbstractModel):
     
     @api.multi
     def render_html(self, data=None):
-        pdb.set_trace()
+        #pdb.set_trace()
         stock = []
         products = self.env['product.product'].search([]).sorted(key=lambda r: r.name)
         for product in products:
             if product.stock_minimo and product.qty_available != 0:
                 print "METELE"
-            if product.stock_minimo >= product.qty_available:
-                stock.append(product.id)
+                if product.stock_minimo >= product.qty_available:
+                    stock.append(product.id)
         #pdb.set_trace() 
         stock_minimo = self.env['product.product'].browse(stock)
 
